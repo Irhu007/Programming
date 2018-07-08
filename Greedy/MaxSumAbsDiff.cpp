4e1fcb1f402d9b5b0a9c47aa1385302e72e6b542
drwhoami <amann12997@gmail.com>
2018-07-09 00:50:03 +0530

    More Practice

--- Greedy/MaxSumAbsDiff.cpp
+++ Greedy/MaxSumAbsDiff.cpp
@@ -1,25 +1,34 @@
 // CPP implementation of
 // above algorithm
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int MaxSumDifference(int a[], int n)
 {
+    vector<int> seq;
     sort(a,a+n);
     
-    int sum=0;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i < n/2; i++)
     {
-        sum+=abs(a[i]-a[n-i-1]);
+        seq.push_back(a[i]);
+        seq.push_back(a[n-i-1]);
     }
+    
+    int sum=0;
+    for(int i = 0;i<n-1;i++)
+    {
+        sum+=abs(seq[i]-seq[i+1]);
+    }
+    sum+=abs(seq[0]-seq[n-1]);
     return sum;
 }
 
 // Driver function
 int main()
 {
-	int a[] = { 1, 2, 4, 8 };
-	int n = sizeof(a) / sizeof(a[0]);
+    int a[] = { 1, 2, 4, 8 };
+    int n = sizeof(a) / sizeof(a[0]);
 
-	cout << MaxSumDifference(a, n) << endl;
+    cout << MaxSumDifference(a, n) << endl;
 }
